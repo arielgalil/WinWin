dce5b5c8a22c81f08efd10f26b535a1534f3b7be
Ariel Galil <arielgalula@gmail.com>
2025-12-26 13:29:35 +0200

    conductor(checkpoint): Checkpoint end of Phase 2: Source Code Relocation

--- utils/lazyImports.ts
+++
@@ -1,5 +0,0 @@
-import { lazy } from 'react';
-
-// Simple lazy loading for admin components
-export const LazyAdminPanel = lazy(() => import('../components/AdminPanel').then(module => ({ default: module.AdminPanel })));
-export const LazyShareableLeaderboard = lazy(() => import('../components/dashboard/ShareableLeaderboard').then(module => ({ default: module.ShareableLeaderboard })));
